592bee786429c317f8f0f131260797db76e26fc8
saxhinrajput2007-ctrl <saxhin.rajput2007@gmail.com>
2026-07-21 13:09:10 +0530

    Initialize Firebase with configuration and exports
    Added Firebase initialization and configuration.

--- firebase.js
+++ firebase.js
@@ -1,1 +1,18 @@
-// Firebase code yahan baad me aayega
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKlI4lNpFWv9c09aro_QGw6oM7nnLz208",
+  authDomain: "olkaa-e2804.firebaseapp.com",
+  projectId: "olkaa-e2804",
+  storageBucket: "olkaa-e2804.firebasestorage.app",
+  messagingSenderId: "333255014176",
+  appId: "1:333255014176:web:f876db7d113eeb749c4945",
+  measurementId: "G-NWW02DCCY6"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getDatabase(app);
