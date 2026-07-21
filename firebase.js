55fa0d196d3fab81a3a532b78d22c62866c66787
saxhinrajput2007-ctrl <saxhin.rajput2007@gmail.com>
2026-07-21 18:56:15 +0530

    Fix formatting and import statements in firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -15,6 +17,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
+
 const db = getFirestore(app);
 
 export { auth, db };
