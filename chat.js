ce1630713bea69c96baab1b958e0a95838fd9137
saxhinrajput2007-ctrl <saxhin.rajput2007@gmail.com>
2026-07-21 19:07:28 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,13 +1,67 @@
-import { auth } from "./firebase.js";
-import { signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { auth, db } from "./firebase.js";
 
-onAuthStateChanged(auth, (user) => {
+import {
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+import {
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const usersDiv = document.getElementById("users");
+
+// Login check
+onAuthStateChanged(auth, async (user) => {
+
   if (!user) {
     window.location.href = "index.html";
+    return;
   }
+
+  loadUsers(user.uid);
+
 });
 
-document.getElementById("logout").addEventListener("click", async () => {
+// Load users
+async function loadUsers(myUid) {
+
+  usersDiv.innerHTML = "";
+
+  const snapshot = await getDocs(collection(db, "users"));
+
+  snapshot.forEach((doc) => {
+
+    const data = doc.data();
+
+    if (data.uid !== myUid) {
+
+      const div = document.createElement("div");
+
+      div.className = "user";
+
+      div.innerHTML = data.email;
+
+      div.onclick = () => {
+
+        alert("Chat with: " + data.email);
+
+      };
+
+      usersDiv.appendChild(div);
+
+    }
+
+  });
+
+}
+
+// Logout
+document.getElementById("logout").onclick = async () => {
+
   await signOut(auth);
+
   window.location.href = "index.html";
-});
+
+};
