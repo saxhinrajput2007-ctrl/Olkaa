66344a9474824b0759af8465654be7e7687a04b1
saxhinrajput2007-ctrl <saxhin.rajput2007@gmail.com>
2026-07-21 13:12:55 +0530

    Implement user signup and login functionality

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
+import { auth } from "./firebase.js";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
+document.getElementById("signup").onclick = () => {
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      document.getElementById("status").innerText = "✅ Sign Up Successful!";
+    })
+    .catch((error) => {
+      document.getElementById("status").innerText = error.message;
+    });
+};
+
+document.getElementById("login").onclick = () => {
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      document.getElementById("status").innerText = "✅ Login Successful!";
+    })
+    .catch((error) => {
+      document.getElementById("status").innerText = error.message;
+    });
+};
