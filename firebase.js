58cc9efb31377b7eb36b90f4940b09ce7a9ae673
saxhinrajput2007-ctrl <saxhin.rajput2007@gmail.com>
2026-07-21 13:40:34 +0530

    Refactor firebase.js to remove database export
    Removed database initialization and export from firebase.js.

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDKlI4lNpFWv9c09aro_QGw6oM7nnLz208",
@@ -13,6 +12,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-export const auth = getAuth(app);
-export const db = getDatabase(app);
+export { auth };
